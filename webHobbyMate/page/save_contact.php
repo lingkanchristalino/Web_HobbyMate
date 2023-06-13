83e105822e3e55e633108e588ca2e8aabcc20a12
Lingkan Christalino <131949438+lingkanchristalino@users.noreply.github.com>
2023-06-13 20:58:56 +0800

    Update save_contact.php

--- webHobbyMate/page/save_contact.php
+++ webHobbyMate/page/save_contact.php
@@ -11,10 +11,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Menyimpan data kontak ke dalam database
     $query = "INSERT INTO contacts (name, email, message) VALUES ('$name', '$email', '$message')";
     if ($conn->query($query) === TRUE) {
-        echo "Pesan berhasil terkirim.";
+        // Mengatur pesan berhasil terkirim sebagai variabel sesi
+        session_start();
+        $_SESSION['success_message'] = true;
+
+        // Mengalihkan kembali ke halaman contact
+        header("Location: Contact.php");
     } else {
         echo "Terjadi kesalahan: " . $conn->error;
     }
+     exit;
 }
 
 // Menutup koneksi database
